97313114c0419770c96948d28e5c4bbd2aa1a97a
Ibnu Putra <ibnu@zipay.id>
2021-06-11 21:22:14 +0700

    add font awesome

--- src/navigator/navigator.js
+++ src/navigator/navigator.js
@@ -1,13 +1,24 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/AntDesign'
 
 const Stack = createStackNavigator();
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
+            <Button title={'test'} onPress={() => navigation.navigate('Profile')} />
+            <Icon name="home" size={30} color="#900" />
+        </View>
+    );
+}
+
+function ProfileScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Profile Screen</Text>
         </View>
     );
 }
@@ -16,6 +27,7 @@ export const AppNavigator = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Profile" component={ProfileScreen} />
         </Stack.Navigator>
     )
 }
