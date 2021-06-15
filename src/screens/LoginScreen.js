8fe51cf16bad73784b342addbac6493495830c21
Ibnu Putra <ibnu@zipay.id>
2021-06-15 18:31:42 +0700

    add google signin function

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,13 +1,46 @@
 import * as React from 'react'
-import { View, Text, SafeAreaView, Image, StatusBar, TextInput } from 'react-native'
+import { Image, StatusBar } from 'react-native'
 import { IMAGES } from '../common/Images'
 import { Colors } from '../styles'
 import Screen from '../components/Screen/Component'
 import { TEXT_NORMAL_BOLD, TEXT_SMALL_BOLD } from '../common/Typography'
 import Button from '../components/Button/Component'
 import styles from './styles/LoginScreen'
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth'
 
 const LoginScreen = ({ navigation }) => {
+
+
+    //configure google signin
+    GoogleSignin.configure({
+        webClientId: '992419438996-nvdutfoq59f3h9e8m4mhdrscrkfosu3b.apps.googleusercontent.com',
+    });
+
+    async function onGoogleButtonPress() {
+        // Get the users ID token
+        const { idToken } = await GoogleSignin.signIn();
+
+        // Create a Google credential with the token
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+        // Sign-in the user with the credential
+        return auth().signInWithCredential(googleCredential);
+    }
+
+    async function handleLoginButton() {
+        console.log('logged in....')
+        await onGoogleButtonPress()
+            .then((credential) => {
+                console.log('Login with google success! credential : ' + credential)
+                navigation.navigate('AppStack')
+            })
+            .catch((err) => {
+                console.log('Login with google failed! error : ' + err)
+            })
+    }
+
+
     return (
         <Screen style={styles.container}>
             <StatusBar backgroundColor={Colors.COLOR_PRIMARY} barStyle={'light-content'} />
@@ -17,7 +50,7 @@ const LoginScreen = ({ navigation }) => {
                 icon={IMAGES.google}
                 buttonStyle={styles.googleButton}
                 textStyle={[{ ...TEXT_NORMAL_BOLD }, styles.googleText]}
-                onPress={() => navigation.navigate('AppStack')} />
+                onPress={() => handleLoginButton()} />
         </Screen>
     )
 }
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -2,11 +2,25 @@ import * as React from 'react'
 import { View, Text, StatusBar } from 'react-native'
 import styles from './styles/HomeScreen'
 import Screen from '../components/Screen/Component'
+import Button from '../components/Button/Component'
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
-const ProfileScreen = () => {
+const ProfileScreen = ({ navigation }) => {
+
+    async function handleLogout() {
+        console.log('Logging out....')
+        try {
+            await GoogleSignin.revokeAccess();
+            await GoogleSignin.signOut();
+            navigation.popToTop()
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <Screen theme={'dark'}>
-            <Text>Profile test test</Text>
+            <Button text='logout' onPress={() => handleLogout()} />
         </Screen>
     )
 }
